8cc52c12f443517343c50aefe2fa2278b01453d6
Hermes Alves <hermes@softagon.com.br>
2026-02-23 15:53:57 -0300

    init

---
+++ app/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="text-xl align-middle text-center text-gray-500" :text="message" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nativescript-vue'
+
+  export default defineComponent({
+    computed: {
+      message() {
+        return "Blank {N}-Vue app";
+      }
+    }
+  });
+</script>
